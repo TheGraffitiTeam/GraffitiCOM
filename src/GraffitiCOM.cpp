0155e5d8f1b638c0dc9268d05554f6599359cb99
cluser404 <30370919+cluser404@users.noreply.github.com>
2024-05-25 08:40:25 +0600

    implemented basic ping with callback functionality

--- src/GraffitiCOM.cpp
+++ src/GraffitiCOM.cpp
@@ -1,1 +1,46 @@
 #include "GraffitiCOM.h"
+
+void gInit(GReg* regPtr, HardwareSerial* serialPtr) {
+    regPtr->hardSerialPtr = serialPtr;
+    regPtr->softSerialPtr = NULL;
+    regPtr->buffer.length = 0;
+    regPtr->buffer.bytes[0] = '\0';
+
+    regPtr->pingCallbackFuncPtr = NULL;
+}
+
+void gBufferUpdate(GBuffer* bufferPtr, HardwareSerial* serialPtr) {
+    bufferPtr->length = serialPtr->readBytes(bufferPtr->bytes, BUFFER_SIZE);
+    bufferPtr->bytes[bufferPtr->length] = '\0';
+}
+
+void gBufferUpdate(GBuffer* bufferPtr, SoftwareSerial* serialPtr) {
+    // pending implementation
+}
+
+int gGetBufferLength(GReg reg) {
+    return reg.buffer.length;
+}
+
+char gGetBufferBytes(GReg reg) {
+    return reg.buffer.bytes;
+}
+
+void gUpdate(GReg* regPtr) {
+    if (regPtr->softSerialPtr == NULL) {
+        gBufferUpdate(&(regPtr->buffer), regPtr->hardSerialPtr);
+    } else {
+        gBufferUpdate(&(regPtr->buffer), regPtr->softSerialPtr);
+    }
+
+    // if first byte of buffer is 0x00 then call pingCallbackFunction
+    if ((regPtr->buffer.bytes)[0] == 0x00) {
+        if (regPtr->pingCallbackFuncPtr != NULL) {
+            (*(regPtr->pingCallbackFuncPtr))();
+        }
+    }
+}
+
+void gOnPing(GReg* regPtr, void (*funcPtr)(void)) {
+    regPtr->pingCallbackFuncPtr = funcPtr;
+}
--- src/GraffitiCOM.h
+++ src/GraffitiCOM.h
@@ -1,6 +1,38 @@
 #ifndef GraffitiCOM_h
 #define GraffitiCOM_h
 
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#define BUFFER_SIZE 0x14 // buffer size is 20 in decimal
 
+typedef struct __GBuffer{
+    int length;
+    char bytes[BUFFER_SIZE];
+} GBuffer;
+
+
+typedef struct __GReg{
+    GBuffer buffer;
+    HardwareSerial* hardSerialPtr;
+    SoftwareSerial* softSerialPtr;
+
+    void (*pingCallbackFuncPtr) (void);
+} GReg;
+
+
+// initializes the registry
+void gInit(GReg* regPtr, HardwareSerial* serialPtr);
+// void gInit(GReg* regPtr, SoftwareSerial* serialPtr);
+
+// This function return a buffer adress having the data and keeping the buffer size to BUFFER_SIZE
+void gBufferUpdate(GBuffer* bufferPtr, HardwareSerial* serialPtr);
+void gBufferUpdate(GBuffer* bufferPtr, SoftwareSerial* serialPtr);
+
+int gGetBufferLength(GReg reg);
+
+
+void gOnPing(GReg* regPtr, void (*funcPtr)(void));
+
+void gUpdate(GReg* regPtr);
 
 #endif
